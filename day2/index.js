2729c637913bf2b132536b2a94d232c50f5ca10d
manishyadav02 <raomanishyadav18@gmail.com>
2024-06-11 10:19:31 +0530

    javascript

---
+++ day2/index.js
@@ -0,0 +1,90 @@
+//const fullname="manish yadav"
+//const fullname="manish";
+//console.log(fullname);
+//let name ='it"s hello';
+//console.log(name);
+//let ischeck=true;
+//console.log(ischeck);
+//let person={"key":10,"str":"hello","bool":true,"fun":"function"}
+//console.log(person["key"]);
+//console.log(person["str"]);
+//console.log(person["bool"]);
+//console.log(person["fun"]);
+//let arr=["hello",10]
+//console.log(arr);
+//console.log(arr[0]);
+//console.log(arr[1]);
+
+//function add(num){
+   // console.log("my pocket money is "+num+" in current time");
+   //console.log("my pocket money is" $(num+num));
+//}
+//add(100);
+
+//return num;
+//}
+//const result = add(100);
+//console.log(result);
+
+/*let age=18
+
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+
+else{
+    console.log("you are not eligible to vote");
+}
+
+let age=18
+
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+    else if(age==18){
+    console.log("you are not eligible to vote");
+}
+    else{
+    console.log("you are not eligible to vote");
+}
+
+let str='Mon';
+
+switch(str){
+    case 'Sun':
+       
+        console.log("today is sunday");
+        break;
+    case 'Mon':
+        console.log("today is monday");
+        break;
+
+    case 'Tue':
+        console.log("today is tuesday");
+        break;
+    case 'Wed':
+        console.log("today is wednesday");
+        break;
+
+    case 'Thu':
+        console.log("today is thursday");
+        break;
+    case 'Fri':
+            console.log("today is friday");
+            break;
+
+    case 'Sat':
+
+             console.log("today is saturday");
+              break;
+    default:
+            console.log("invalid day");
+            break;
+}*/
+
+//LOOP
+ 
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
